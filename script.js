f2ab071d34c02350c004417f1357ce816d0ce324
Kayla Wilson <kaylazwilsonbusiness@gmail.com>
2024-11-07 10:35:07 -0500

    phew almost done

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-//const all 
 const heading = document.querySelector('#heading');
 const subheading = document.querySelector('#subheading');
-const replyb =  document.querySelector('#reply-button');
+const replyb = document.querySelector('#reply-button');
 const replym = document.querySelector('#reply-message');
-const sendButton =  document.querySelector('#send-button');
-const cancelButton =  document.querySelector('#cancel-button');
+const sendButton = document.querySelector('#send-button');
+const cancelButton = document.querySelector('#cancel-button');
 const openButton = document.querySelector('#open-button');
+const xButton = document.querySelector('#close');  
+const inboxMessage = document.querySelector('#inbox-message');
+const markButton = document.querySelector('#mark-button'); // "Mark as Unread" button
 
 // head func
-const changeFontColor = () =>{
-    heading.classList.toggle ("blue-text");
+const changeFontColor = () => {
+    heading.classList.toggle("blue-text");
 }
-
-heading.addEventListener("click",changeFontColor);
-
-
-//subheading func 
-
+heading.addEventListener("click", changeFontColor);
+// sub func
 const changeSubColor = () => {
-    subheading.classList.toggle ("blue-text");
+    subheading.classList.toggle("blue-text");
 }
-
 subheading.addEventListener("click", changeSubColor);
 
-// reply (remove hidden)
-
-
-const openReplyMessage  = () => {
+// reply
+const openReplyMessage = () => {
     replym.classList.remove('hidden');
-
 }
+replyb.addEventListener("click", openReplyMessage);
 
-replyb.addEventListener("click",openReplyMessage);
-
-// send/ cancel buttons 
-
+// Send/cancel buttons 
 const closeReplyMessage = () => {
     replym.classList.add('hidden');
 }
 sendButton.addEventListener('click', closeReplyMessage);
-cancelButton.addEventListener('click',closeReplyMessage);
+cancelButton.addEventListener('click', closeReplyMessage);
 
+// indox 
+const closeInboxMessage = () => {
+    inboxMessage.classList.add('hidden'); 
+
+    
+    replyb.classList.add('hidden');
+    markButton.classList.add('hidden');
+}
+xButton.addEventListener('click', closeInboxMessage);
+
+// open inbox message 
+const openInboxMessage = () => {
+    inboxMessage.classList.remove('hidden');  
+    replyb.classList.remove('hidden');
+    markButton.classList.remove('hidden');
+}
+openButton.addEventListener('click', openInboxMessage);
+
+// unread func
+const markAsUnread = () => {
+    inboxMessage.classList.toggle('is-read'); 
+    if (inboxMessage.classList.contains('is-read')) {
+        markButton.textContent = 'Mark as Read'; 
+    } else {
+        markButton.textContent = 'Mark as Unread'; 
+    }
+}
+markButton.addEventListener('click', markAsUnread);
